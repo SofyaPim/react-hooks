b90dab0eaebcdc6db380cf46d1e93a05788a6bac
Sofya <48826470+SofyaPim@users.noreply.github.com>
2020-04-19 13:35:00 +0300

    Updates

---
+++ precache-manifest.a9265ab9d866c5565a6a923c3bfda9ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "314b16673e2a75b1d54f89eeec6087df",
+    "url": "/SofyaPim/react-hooks.git/index.html"
+  },
+  {
+    "revision": "57164cf685fe84755a45",
+    "url": "/SofyaPim/react-hooks.git/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "e37f604a32100f0e5483",
+    "url": "/SofyaPim/react-hooks.git/static/css/main.e3943d16.chunk.css"
+  },
+  {
+    "revision": "57164cf685fe84755a45",
+    "url": "/SofyaPim/react-hooks.git/static/js/2.aa50909d.chunk.js"
+  },
+  {
+    "revision": "b3924902b6517db7f2b56facf8ba237b",
+    "url": "/SofyaPim/react-hooks.git/static/js/2.aa50909d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e37f604a32100f0e5483",
+    "url": "/SofyaPim/react-hooks.git/static/js/main.754bbfd1.chunk.js"
+  },
+  {
+    "revision": "b0b1624227cfbd9874da",
+    "url": "/SofyaPim/react-hooks.git/static/js/runtime-main.0be1c545.js"
+  }
+]);
